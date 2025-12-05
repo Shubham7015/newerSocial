f898783980d3489a0c306a5ea6377890b345303c
Shubham Rohilla <srohilla099@gmail.com>
2025-12-05 23:16:38 +0530

    add enviornment variable

--- newerSocial/src/conf/conf.js
+++ newerSocial/src/conf/conf.js
@@ -5,7 +5,7 @@ const conf = {
   appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
   appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
   appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-  appwriteVerificationUrl: String(import.meta.env.VITE_APPWRITE_VERIFICATION_URL),
+  appwriteVerificationUrl: String(import.meta.env.VITE_APPWRITE_VERIFICATION_URL || ""),
   tinymceApiKey: String(import.meta.env.VITE_TINYMCE_API_KEY),
 };
 export default conf;
